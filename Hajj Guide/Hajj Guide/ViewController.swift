3cf0d23cad32d6fb2396cf44e7b8e9ac7ead5f80
raad1415 <34250168+raad1415@users.noreply.github.com>
2018-08-02 19:05:20 +0300

    V1.01
    Initial commit

---
+++ Hajj Guide/Hajj Guide/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Hajj Guide
+//
+//  Created by Raad Alkhaldi on 19/11/1439 AH.
+//  Copyright © 1439 Raad Alkhaldi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
